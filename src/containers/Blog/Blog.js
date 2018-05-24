3f2cd32e09bbd9074478ae73ba0643d1ad7ff056
Angel Ancheta <angelancheta98@yahoo.com>
2018-05-24 14:33:47 -0700

    'half-working'

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+// import axios from "axios";
+// import axios from '../../axios'
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
+
+import "./Blog.css";
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
+
+class Blog extends Component {
+  state = {
+    auth: false
+  }
+
+  render() {
+
+    return (
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li><NavLink to="/posts/" exact>Posts</NavLink></li>
+              <li><NavLink to={{pathname: '/new-post', hash: '#submit', search: '?quick-submit=true'}}>New Post</NavLink></li>
+            </ul>
+          </nav>
+        </header>
+        {/* <Route path="/" exact render={() => <h1>Homeeeee</h1>}/>
+        <Route path="/" exact render={() => <h1>Homeeee asdfasfe</h1>}/> */}\
+        <Switch>
+        {this.state.auth ? <Route path="/new-post" exact component={NewPost} /> : null}
+        <Route path="/posts/" component={Posts} />
+        <Redirect from="/" to="/posts/" />
+        {/* <Route path="/" component={Posts} /> */}
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default Blog;
